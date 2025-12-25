61f14a547d7fe4d90ea6152b2237c5a33adc224b
BTCRam <ramyarajamanickam@gmail.com>
2025-12-26 00:21:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,13 +110,13 @@ st.subheader("Privacy Index Comparison")
 st.bar_chart(df_privacy.set_index("Method")["Privacy Index"])
 
 st.subheader("Smart Contract Gas Consumption (ETH)")
-st.bar_chart(df_gas.set_index("Method")["Smart Contract Gas Consumption (ETH)"])
+st.bar_chart(df_gas.set_index("Method")["Gas Consumption (ETH)"])
 
 st.subheader("Smart Contract Throughput (tx/block)")
-st.bar_chart(df_throughput.set_index("Method")["Smart Contract Throughput (tx/block)"])
+st.bar_chart(df_throughput.set_index("Method")[" Throughput (tx/block)"])
 
 st.subheader("Smart Contract Execution Latency (ms)")
-st.bar_chart(df_time.set_index("Method")["Smart Contract Execution Latency (ms)"])
+st.bar_chart(df_time.set_index("Method")["Execution Latency (ms)"])
 
 # --------------------------------------------------
 # Result Interpretation
@@ -132,6 +132,7 @@ st.caption(
     "Note: Experimental values correspond to controlled smart contract executions "
     "performed on a private blockchain test environment."
 )
+
 
 
 
